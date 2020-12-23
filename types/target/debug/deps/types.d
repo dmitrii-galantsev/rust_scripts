6c1d8d0f7e0a8df72c8a9126473075ad700bb380
Dmitrii Galantsev <dgalants@indiana.edu>
2020-12-23 11:32:09 -0500

    rm target

--- types/target/debug/deps/types.d
+++
@@ -1,5 +0,0 @@
-/Users/dmitriigalantsev/local/scripts/rust_scripts/types/target/debug/deps/types: src/main.rs
-
-/Users/dmitriigalantsev/local/scripts/rust_scripts/types/target/debug/deps/types.d: src/main.rs
-
-src/main.rs:
--- types/target/debug/types.d
+++
@@ -1,1 +0,0 @@
-/Users/dmitriigalantsev/local/scripts/rust_scripts/types/target/debug/types: /Users/dmitriigalantsev/local/scripts/rust_scripts/types/src/main.rs
