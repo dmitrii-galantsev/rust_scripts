0fb5107f6edb2f16ce7a40dde240f9aeb659bd24
Dmitrii Galantsev <dgalants@indiana.edu>
2020-12-26 02:45:02 -0500

    cleanup

--- tuple/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn println(tuple: (i32, &str, f32)) {
-    println!("i32: {}, str: {}, f32: {}", tuple.0, tuple.1, tuple.2);
-    println!("{:?}", tuple);
-}
-
-fn main() {
-    let x: (i32, &str, f32) = (420, "owo", 0.69);
-    println(x);
-}
